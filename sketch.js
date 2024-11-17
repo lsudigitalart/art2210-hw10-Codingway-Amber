2fe3291b5b51be59af327417921c38d81efc4e53
Codingway-Amber <amberreine7@gmail.com>
2024-11-17 13:06:21 -0600

    added rectangles

--- sketch.js
+++ sketch.js
@@ -1,10 +1,18 @@
-var myArray = ["lol", "lmao", "xD"]
+var lan = [];
 
 function setup() {
-  createCanvas(400, 400);
-  print(myArray[0])
+ createCanvas(400, 200);
+ noStroke();
+ fill(237, 147, 63);
+ for (var i = 0; i < 3000; i++) {
+ lan[i] = random(-1000, 200);
+ }
 }
-
 function draw() {
-  background(220);
+ background(0);
+ for (var i = 0; i < lan.length; i++) {
+ lan[i] += 0.5;
+ var x = i * 4;
+ rect(x, lan[i], 35, 45, 8);
+ }
 }
